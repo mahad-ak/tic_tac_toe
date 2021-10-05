f67fc7a1d802e646fa8a08856d86071717fab9fb
mahad-ak <mahadali200@gmail.com>
2021-10-05 22:59:55 +0500

    first commit

---
+++ my_tic_tac_toe.py
@@ -0,0 +1,7 @@
+# Python Tic-Tac-Toe
+
+# This is the tic tac toe game board made using a dictionary in which they keys represent the board slots
+# and the corresponding values contain either the slot number or the player symbol (X, O). 
+game_board = {'slot1': '1' , 'slot2': '2' , 'slot3': '3' ,
+            'slot4': '4' , 'slot5': '5' , 'slot6': '6' ,
+            'slot7': '7' , 'slot8': '8' , 'slot9': '9' }
