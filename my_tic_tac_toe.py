f64695433e955f1b69da63ff2aa4073fcaab766a
mahad-ak <mahadali200@gmail.com>
2021-10-05 23:02:25 +0500

    added functions to print board

--- my_tic_tac_toe.py
+++ my_tic_tac_toe.py
@@ -5,3 +5,28 @@
 game_board = {'slot1': '1' , 'slot2': '2' , 'slot3': '3' ,
             'slot4': '4' , 'slot5': '5' , 'slot6': '6' ,
             'slot7': '7' , 'slot8': '8' , 'slot9': '9' }
+
+
+# This function operates the game
+def tic_tac_toe():
+
+    # The game is started by player 'X'
+    player = 'X' 
+    total_turns=9 #The total number of possible turns in tic tac toe 9.
+
+    # This counts the number of successful turns.
+    turns_completed=0
+
+            
+
+
+# This function checks if a player's move passed in as input is a legal move and executes it, otherwise prints an error message and re-prompts the user.
+def check_move(player, game_board):
+    return 2
+
+# Prints the game board
+def show_tic_tac_toe(game_board):
+    return 1
+
+if __name__ == "__main__":
+    tic_tac_toe()
